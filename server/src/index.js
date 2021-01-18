1a311e2c31e91df6622b7187b2cb9fa7bf429fa0
Gianni Narracci <gnarracci@gmail.com>
2021-01-18 10:17:30 -0400

    Server Main function Setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,8 @@
 const app = require('./app');
 
-app.listen(4000, () => {
-    console.log("Server on port 4000")
-});
+async function main() {
+    await app.listen(4000);
+    console.log("Server on port 4000");
+}
+
+main();
