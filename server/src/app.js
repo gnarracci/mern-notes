cf0eefb7febab202821c75af873d8ab76b41b18f
Gianni Narracci <gnarracci@gmail.com>
2021-01-25 23:33:37 -0400

    Server State Viewers Config

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 
 // settings
 app.set('port', 4000);
 
 // middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
 // routes
+app.get('/api/users', (req, res) => res.send('Users Routes'));
+app.get('/api/notes', (req, res) => res.send('Notes Routes'));
 
 module.exports = app;
