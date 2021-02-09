448ab64e22f8c2ce40cf5fd34c56c127383c2b19
Gianni Narracci <gnarracci@gmail.com>
2021-02-09 06:09:33 -0400

    React Routes Config

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Navigation from './components/Navigation';
+import CreateUser from './components/CreateUser';
+import CreateNote from './components/CreateNote';
+import NotesList from './components/NotesList';
 
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      <Navigation />
+      Hello World
+    </div>
   );
 }
 
