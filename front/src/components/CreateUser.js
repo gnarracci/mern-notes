db327a2b52d4f8d98fd8bc12ddfaed04d263ed32
Gianni Narracci <gnarracci@gmail.com>
2021-02-10 23:47:17 -0400

    Axios Request Setup

--- front/src/components/CreateUser.js
+++ front/src/components/CreateUser.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class CreateUser extends Component {
+
+    state = {
+        users: []
+    }
+    
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/users');
+        this.setState({users: res.data});
+        console.log(this.state.users);
+    }
+
     render() {
         return (
-            <div>
-                CREATE USER
+            <div className="row">
+                <div className="col-md-4">
+                    form user
+                </div>
+                <div className="col-md-8">
+                    <ul className="list-group">
+                        {
+                            this.state.users.map(user => <li className="list-group-item list-group-item-action">
+                                {user.username}
+                            </li>)
+                        }
+                    </ul>
+                </div>
             </div>
         )
     }
