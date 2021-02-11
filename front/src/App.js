0b14ea88602be9f5b1c8923ac6c5cacdce3c427e
Gianni Narracci <gnarracci@gmail.com>
2021-02-11 05:10:14 -0400

    Get, Post & Delete Ops Setup

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootswatch/dist/cosmo/bootstrap.min.css';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/cosmo/bootstrap.min.css";
 
-import Navigation from './components/Navigation';
-import CreateUser from './components/CreateUser';
-import CreateNote from './components/CreateNote';
-import NotesList from './components/NotesList';
+import Navigation from "./components/Navigation";
+import CreateUser from "./components/CreateUser";
+import CreateNote from "./components/CreateNote";
+import NotesList from "./components/NotesList";
 
 function App() {
   return (
     <Router>
       <Navigation />
-      <Route path="/" exact component={NotesList} />
-      <Route path="/edit/:id" component={CreateNote} />
-      <Route path="/create" component={CreateNote} />
-      <Route path="/user" component={CreateUser} />      
+      <div className="container p-4">
+        <Route path="/" exact component={NotesList} />
+        <Route path="/edit/:id" component={CreateNote} />
+        <Route path="/create" component={CreateNote} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
