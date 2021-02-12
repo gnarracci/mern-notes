a6d3aa0558ef14205845e0828f9255bd94cf7a0f
Gianni Narracci <gnarracci@gmail.com>
2021-02-12 05:27:13 -0400

    UI Gradient CSS Added

--- front/src/components/CreateNote.js
+++ front/src/components/CreateNote.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 export default class CreateNote extends Component {
     render() {
         return (
-            <div>
-                CREATE NOTES
-            </div>
+           <div className="col-md-6 offset-md-3">
+               <div className="card card-body">
+                   <h4>Create a Note</h4>
+                   <form onSubmit={}>
+
+                        <button>
+                            
+                        </button>
+                   </form>
+               </div>
+           </div>
         )
     }
 }
