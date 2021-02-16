6ee8790d223f2f084a2ef5a99670c492340a20e1
Gianni Narracci <gnarracci@gmail.com>
2021-02-16 16:44:19 -0400

    Create Note Component Setup

--- front/src/components/CreateNote.js
+++ front/src/components/CreateNote.js
@@ -6,7 +6,7 @@ export default class CreateNote extends Component {
            <div className="col-md-6 offset-md-3">
                <div className="card card-body">
                    <h4>Create a Note</h4>
-                   <form onSubmit={}>
+                   <form onSubmit={this.is}>
 
                         <button>
                             
