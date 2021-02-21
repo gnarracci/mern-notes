009c4f0b0792730f405eda33368fd24c10226203
Gianni Narracci <gnarracci@gmail.com>
2021-02-21 17:31:33 -0400

    Create Note User Data get options

--- front/src/components/CreateNote.js
+++ front/src/components/CreateNote.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class CreateNote extends Component {
+
+    state = {
+        users: []
+    };
+
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/users');
+        this.setState({ users: res.data })
+        console.log(res.data);
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+    }
+
+
+
     render() {
         return (
-           <div className="col-md-6 offset-md-3">
-               <div className="card card-body">
-                   <h4>Create a Note</h4>
-                   <form onSubmit={this.is}>
-
-                        <button>
-                            
+            <div className="col-md-6 offset-md-3">
+                <div className="card card-body">
+                    <h4>Create a Note</h4>
+                        {/** SELECT USER */}
+                    <div className="form-group">
+                        <select className="form-control" name="userSelected">
+                            {
+                                this.state.users.map(user => <option key={user._id}>
+                                    {user.username}
+                                </option>)
+                            }
+                        </select>
+                    </div>
+                    <form onSubmit={this.onSubmit}>
+                        <button type="submit" className="btn btn-primary">
+                            Save
                         </button>
-                   </form>
-               </div>
-           </div>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
